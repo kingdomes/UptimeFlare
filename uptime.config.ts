98006cbcef678759d315609cbb6d48ee9f253529
kingdomes <diqiuzhiguang@hotmail.com>
2025-07-03 14:57:14 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,22 +1,31 @@
-// Clean, deployable Uptimeflare configuration
-import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config';
+import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
 
 const pageConfig: PageConfig = {
-  title: '服务状态页面',
+  // Title for your status page
+  title: "服务状态页面",
+  // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
     { link: 'https://github.com/kingdomes', label: 'GitHub' },
-    { link: 'https://115411.xyz/', label: 'Blog' },
+    { link: 'https://115411.xyz', label: 'Blog' },
     { link: 'mailto:admin@115411.xyz', label: 'Email Me', highlight: true },
   ],
+  // [OPTIONAL] Group your monitors
+  // If not specified, all monitors will be shown in a single list
+  // If specified, monitors will be grouped and ordered, not-listed monitors will be invisble (but still monitored)
   group: {
     '🌐 Public': ['campus_wall', 'blog_monitor', 'uptimeflare_monitor'],
     '🔐 Private': ['router_ssh'],
   },
-};
+}
 
 const workerConfig: WorkerConfig = {
+  // Write KV at most every 3 minutes unless the status changed
   kvWriteCooldownMinutes: 3,
+  // Enable HTTP Basic auth for status page & API by uncommenting the line below, format `<USERNAME>:<PASSWORD>`
+  // passwordProtection: 'username:password',
+  // Define all your monitors here
   monitors: [
+    // Example HTTP Monitor
     {
       id: 'campus_wall',
       name: '🧱 校园墙',
@@ -59,6 +68,7 @@ const workerConfig: WorkerConfig = {
       responseForbiddenKeyword: 'bad gateway',
       checkProxyFallback: true,
     },
+    // Example TCP Monitor
     {
       id: 'router_ssh',
       name: '🔐 Router SSH',
@@ -70,31 +80,70 @@ const workerConfig: WorkerConfig = {
     },
   ],
   notification: {
+    // [Optional] apprise API server URL
+    // if not specified, no notification will be sent
     appriseApiServer: 'https://apprise.example.com/notify',
+    // [Optional] recipient URL for apprise, refer to https://github.com/caronc/apprise
+    // if not specified, no notification will be sent
     recipientUrl: 'tgram://bottoken/ChatID',
+    // [Optional] timezone used in notification messages, default to "Etc/GMT"
     timeZone: 'Asia/Shanghai',
+    // [Optional] grace period in minutes before sending a notification
+    // notification will be sent only if the monitor is down for N continuous checks after the initial failure
+    // if not specified, notification will be sent immediately
     gracePeriod: 5,
-    skipNotificationIds: [],
+    // [Optional] disable notification for monitors with specified ids
+    skipNotificationIds: ['foo_monitor', 'bar_monitor'],
   },
   callbacks: {
-    onStatusChange: async (env, monitor, isUp, timeIncidentStart, timeNow, reason) => {
-      // 可用于日志上报或自定义推送
+    onStatusChange: async (
+      env: any,
+      monitor: any,
+      isUp: boolean,
+      timeIncidentStart: number,
+      timeNow: number,
+      reason: string
+    ) => {
+      // This callback will be called when there's a status change for any monitor
+      // Write any Typescript code here
+      // This will not follow the grace period settings and will be called immediately when the status changes
+      // You need to handle the grace period manually if you want to implement it
     },
-    onIncident: async (env, monitor, timeIncidentStart, timeNow, reason) => {
-      // 可用于周期性提醒持久故障
+    onIncident: async (
+      env: any,
+      monitor: any,
+      timeIncidentStart: number,
+      timeNow: number,
+      reason: string
+    ) => {
+      // This callback will be called EVERY 1 MINTUE if there's an on-going incident for any monitor
+      // Write any Typescript code here
     },
   },
-};
+}
 
+// You can define multiple maintenances here
+// During maintenance, an alert will be shown at status page
+// Also, related downtime notifications will be skipped (if any)
+// Of course, you can leave it empty if you don't need this feature
+// const maintenances: MaintenanceConfig[] = []
 const maintenances: MaintenanceConfig[] = [
   {
-    monitors: ['campus_wall', 'blog_monitor'],
-    title: '服务器维护公告',
-    body: '例行安全更新与系统优化维护期间，部分服务可能短暂中断。',
+    // [Optional] Monitor IDs to be affected by this maintenance
+    monitors: ['foo_monitor', 'bar_monitor'],
+    // [Optional] default to "Scheduled Maintenance" if not specified
+    title: 'Test Maintenance',
+    // Description of the maintenance, will be shown at status page
+    body: 'This is a test maintenance, server software upgrade',
+    // Start time of the maintenance, in UNIX timestamp or ISO 8601 format
     start: '2025-04-27T00:00:00+08:00',
+    // [Optional] end time of the maintenance, in UNIX timestamp or ISO 8601 format
+    // if not specified, the maintenance will be considered as on-going
     end: '2025-04-30T00:00:00+08:00',
+    // [Optional] color of the maintenance alert at status page, default to "yellow"
     color: 'blue',
   },
-];
+]
 
-export { pageConfig, workerConfig, maintenances };
+// Don't forget this, otherwise compilation fails.
+export { pageConfig, workerConfig, maintenances }
